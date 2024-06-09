5a60f7300aac0f5423e072934045caff74f45720
devsimiyu <dev.wrodgerss@gmail.com>
2024-06-09 23:50:49 +0300

    Queue messages to Notifications service with azurite

---
+++ ServiceDesk.Notifications/Service/QueueWorkerService.cs
@@ -0,0 +1,22 @@
+﻿
+using ServiceDesk.Util;
+
+namespace ServiceDesk.Notifications.Service;
+
+public class QueueWorkerService(ILogger<QueueWorkerService> logger, QueueService queue) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var message = await queue.GetMessageAsync();
+
+            if (message != null)
+            {
+                logger.LogInformation("Received message - {Message}", message);
+            }
+        }
+
+        logger.LogInformation("Closing queue worker");
+    }
+}
---
+++ ServiceDesk.Util/Service/QueueService.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace ServiceDesk.Util;
+
+public class QueueService
+{
+    private readonly QueueClient _client;
+
+    public QueueService()
+    {
+        _client = new QueueClient("UseDevelopmentStorage=true", "notifications-queue", new QueueClientOptions
+        {
+            MessageEncoding = QueueMessageEncoding.Base64
+        });
+    }
+
+    public async Task<string?> GetMessageAsync()
+    {
+        QueueProperties props = await _client.GetPropertiesAsync();
+
+        if (props.ApproximateMessagesCount == 0)
+        {
+            return null;
+        }
+        
+        QueueMessage message = await _client.ReceiveMessageAsync();
+        string text = message.MessageText;
+
+        await DeleteMessageAsync(message);
+
+        return text;
+    }
+    
+    public async Task DeleteMessageAsync(QueueMessage message)
+    {
+        await _client.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+    }
+
+    public async Task SendMessageAsync(object message)
+    {
+        var text = JsonSerializer.Serialize(message);
+
+        await _client.SendMessageAsync(text);
+    }
+}
