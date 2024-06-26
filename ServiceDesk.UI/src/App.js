b66efa7fe79ad8aa180c55d87efb22d744c0eaa4
devsimiyu <dev.wrodgerss@gmail.com>
2024-06-26 22:26:08 +0300

    Add react ui project

---
+++ ServiceDesk.UI/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import Dashboard from './components/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
